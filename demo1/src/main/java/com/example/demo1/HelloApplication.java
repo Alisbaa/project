6234e82276e7bbcf601bd2b4bcfd20c8101e17b0
Alisbaa <alisaasila2004@gmail.com>
2023-05-17 18:15:10 +0600

    application

--- demo1/src/main/java/com/example/demo1/HelloApplication.java
+++ demo1/src/main/java/com/example/demo1/HelloApplication.java
@@ -1,18 +1,18 @@
 package com.example.demo1;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("flappy.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getRoot().requestFocus();
+        stage.setTitle("Flappy Bird!");
         stage.setScene(scene);
         stage.show();
     }
